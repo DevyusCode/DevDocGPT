315b0270aea8d4a48b6748fadbcda6f4339c5e5b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-05-07 17:32:04 +0200

    Added new packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import removeImports from "next-remove-imports"
+
+/** @type {function(import("next").NextConfig): import("next").NextConfig}} */
+const removeImportsFun = removeImports({
+  test: /node_modules([\s\S]*?)\.(tsx|ts|js|mjs|jsx)$/,
+  matchImports: "\\.(less|css|scss|sass|styl)$",
+})
+
+export default removeImportsFun({
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-}
-
-export default nextConfig
+})
